4e51d1237b2d421fa0ae7629a822171009dd2f35
Aaron B. <aaron.bottner@mymail.champlain.edu>
2026-08-13 21:49:45 -0500

    Update bike.js

--- bike.js
+++ bike.js
@@ -1,11 +1,33 @@
-import React, { useState } from "react";
+const { useState } = React;
 
-const [position, setPosition] = useState(0);
+function App() {
+    const [position, setPosition] = useState(0);
 
-function moveBike() {
-  if (position >= 150) {
-    setPosition(0);
-  } else {
-    setPosition(position + 50);
-  }
+    function moveBike() {
+        if (position >= 150) {
+            setPosition(0);
+        } else {
+            setPosition(position + 50);
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={moveBike}>
+                Move Bike
+            </button>
+
+            <img
+                id="Bike"
+                src="images/bike.jpg"
+                alt="Black bike"
+                className={`position-${position}`}
+            />
+        </div>
+    );
 }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("bike-root")
+);
